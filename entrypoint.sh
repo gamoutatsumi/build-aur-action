78c803af7f8798408ec328afc8a3719a530326ea
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2021-12-21 10:23:23 +0900

    add support for other repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-git clone "https://aur.archlinux.org/$1.git"
-cd "$1"
+REPO=$1
+if [[ $1 =~ .*/.* ]]; then
+  git clone "https://$REPO.git" || exit 1
+else
+  git clone "https://aur.archlinux.org/$REPO.git" || exit 1
+fi
+cd "$(basename "${REPO}")" || exit 1
 makepkg -sf --noconfirm --skippgpcheck
